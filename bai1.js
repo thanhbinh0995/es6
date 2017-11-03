7fc4f3208f55fbf3d7e99fdaa92ec6d422675c47
Binh Pham <thanhbinh0995@gmail.com>
2017-11-03 17:20:53 +0700

    remove log bai1

--- bai1.js
+++ bai1.js
@@ -2,10 +2,4 @@
 // var date = prompt("Enter your birthday: ");
 
 import {calculateAge, log} from './actions';
-import moment from 'moment';
 
-let res = calculateAge();
-document.getElementById("solution1").innerHTML = document.getElementById("date").value;;
-log("Cau 1", true);
-log(`so tuoi hien tai: ${res}`);
-log("Het cau 1", true);
